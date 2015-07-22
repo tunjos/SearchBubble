581dbf86bc2a4fef1e2b28f3dd7f3bfb553e6a3c
Tunji Olu-Taiwo <tunjos@tunjos.co>
2015-07-22 06:49:09 +0200

    upgrade to AppIntro 3.1.0

--- app/src/main/java/com/tunjos/searchbubble/activities/TutorialActivity.java
+++ app/src/main/java/com/tunjos/searchbubble/activities/TutorialActivity.java
@@ -1,30 +1,23 @@
 package com.tunjos.searchbubble.activities;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
-import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.tunjos.searchbubble.R;
-import com.tunjos.searchbubble.fragments.SlideFragment;
 
-public class TutorialActivity extends AppIntro {
+public class TutorialActivity extends AppIntro2 {
 
     @Override
     public void init(Bundle savedInstanceState) {
 
-        addSlide(SlideFragment.newInstance(R.layout.fragment_slide1));
-        addSlide(SlideFragment.newInstance(R.layout.fragment_slide2));
-        addSlide(SlideFragment.newInstance(R.layout.fragment_slide3));
-        addSlide(SlideFragment.newInstance(R.layout.fragment_slide4));
+        int sbRedColor = getResources().getColor(R.color.sb_red);
 
-        setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));
-
-        showSkipButton(false);
-    }
-
-    @Override
-    public void onSkipPressed() {
+        addSlide(AppIntroFragment.newInstance(getString(R.string.tx_slide_title_1), getString(R.string.tx_slide_description_1), R.drawable.ic_rate, sbRedColor));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.tx_slide_title_2), getString(R.string.tx_slide_description_2), R.drawable.ic_rate, sbRedColor));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.tx_slide_title_3), getString(R.string.tx_slide_description_3), R.drawable.ic_rate, sbRedColor));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.tx_slide_title_4), getString(R.string.tx_slide_description_4), R.drawable.ic_rate, sbRedColor));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.tx_slide_title_5), getString(R.string.tx_slide_description_5), R.drawable.ic_link, sbRedColor));
     }
 
     @Override
